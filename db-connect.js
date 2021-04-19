91586d2ce5ea7db30e68fb0168d5c39ae0f88c25
wt-dtop <wcolet@gmail.com>
2021-04-18 21:42:45 -0400

    added pg package

---
+++ db-connect.js
@@ -0,0 +1,27 @@
+const { Client } = require('pg');
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'testdb',
+    password: 'poophead',
+    port: 5432,
+});
+
+const query = `
+CREATE TABLE users (
+    email varchar,
+    firstName varchar,
+    lastName varchar,
+    age int
+);
+`;
+
+client.query(query, (err, res) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Table is successfully created');
+    client.end();
+});
