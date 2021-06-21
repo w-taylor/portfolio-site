516497c78d1385c5094280f1165af3e067bbaa75
William <wcolet@gmail.com>
2021-06-21 01:29:05 +0000

    message pruning added to wall app

--- add-row.js
+++ add-row.js
@@ -1,5 +1,4 @@
-async function addRow(date,postBody) {
-	try {
+function addRow(date,postBody) {
 	const { Pool } = require('pg');
 	let query = {
         text: 'INSERT INTO testtwo(age,pbody) VALUES($1,$2) RETURNING *',
@@ -22,23 +21,21 @@ async function addRow(date,postBody) {
 	};
 	
 	console.log("count get start");
-	let posts = await (async function () {
-		pool.query(query, (err,res) => {
-			if (err) {
-				console.log(err.stack);
-				return -1;
-			} else {
-				let posts = res.rows;
-				console.log(posts);
-				console.log(posts.length);
-				if (posts.length < 20) {return -1};
-				return posts;
-			}
-		})
-	})();
+	pool.query(query, (err,res) => {
+		if (err) {
+			console.log(err.stack);
+		} else {
+			let posts = res.rows;
+			console.log(posts);
+			console.log(posts.length);
+			if (posts.length < 10) {return};
+			prunePost(posts, pool);
+		}
+	});
+};
 	
-	console.log(posts);
 	
+function prunePost(posts, pool){
 	let target = parseInt(posts[0].age);
 	for (let x = 0; x < posts.length; x++) {
 		let cand = parseInt(posts[x].age);
@@ -61,9 +58,6 @@ async function addRow(date,postBody) {
 			console.log(res.rows)
 		}
 	});
-	} catch(err) {
-		console.log(err);
-	};
 };
 
 module.exports = {
