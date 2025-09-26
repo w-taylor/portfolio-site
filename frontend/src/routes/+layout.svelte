27177d7398a578986ceb6d11ce905675ce22970e
William Taylor <wcolet@gmail.com>
2025-09-26 14:24:20 -0500

    simplified header css

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="header-wrapper">
-  <div class="header-left"><h1 class="site-name">wtaylor.xyz</h1></div>
+  <div class="header-left"><strong>wtaylor.xyz</strong></div>
   <div class="header-right"><nav>
     <a href="#projects-header">Projects</a> | 
     <a href="#site-info-header">Site Info</a> | 
@@ -34,31 +34,23 @@
 
 <style>
   .header-wrapper {
-    box-sizing: border-box;
     width: 100%;
     margin: 0;
-    position: absolute;
-    display: grid;
-    grid-template-columns: 30%;
-    grid-auto-rows: 2em;
+    background-color: white;
     color: black;
+    display: grid; 
+    grid-template-columns: 1fr 1fr;
+    height: 5em;
   }
 
   .header-left {
-    grid-column: 1 / 2;
-    grid-row: 1 / 3;
-    clip-path: polygon(0 0,100% 0,80% 100%,0 100%);
-    background-color: white;
-    color: black;
+    font-size: 3em;
+    margin: auto 0.5em;
   }
 
   .header-right {
     display: flex;
     justify-content: flex-end;
-    grid-column: 1 / 4;
-    grid-row: 1;
-    background-color: white;
-    color: black;
   }
 
   .header-spacer {
@@ -99,12 +91,9 @@
     height: 8em;
   }
 
-  .site-name{
-    font-size: 2em;
-  }
-
-  .site-name, nav{
-    margin: .25em .5em;
+  nav{
+    margin: auto 2em;
+    font-size: 1.5em;
   }
 
 </style>
