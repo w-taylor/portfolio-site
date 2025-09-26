c3342ae88ea8951a761d45f7dee548aabc3a83c5
William Taylor <wcolet@gmail.com>
2025-09-26 14:08:28 -0500

    added footer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -15,6 +15,23 @@
 
 <slot />
 
+<div class="footer-spacer"></div>
+
+<div class="footer-wrapper">
+  <div class="footer-left">
+    <div class="footer-left-text">
+      <div style="font-size: 3em;"><strong>wtaylor.xyz</strong></div>
+      <div>© 2025 William Taylor</div>
+    </div>
+  </div>
+  <div class="footer-right">
+    <a href="" class="footer-link">Home</a>
+    <a href="#projects-header" class="footer-link">Projects</a>
+    <a href="#site-info-header" class="footer-link">Site Info</a>
+    <a href="#contact-header" class="footer-link">Contact</a>
+  </div>
+</div>
+
 <style>
   .header-wrapper {
     box-sizing: border-box;
@@ -46,6 +63,40 @@
 
   .header-spacer {
     height: 7em;
+  }
+
+  .footer-wrapper {
+    background-color: white;
+    color: black;
+    display: grid; 
+    grid-template-columns: 1fr 1fr;
+    height: 8em;
+  }
+
+  .footer-left {
+    margin: auto 0;
+    border-right: .05em solid black; 
+    height: 6em;
+    text-align: right;
+  }
+
+  .footer-left-text {
+    display: block;
+    padding-right: 2.5em;
+  }
+
+  .footer-right {
+    margin: auto 0;
+  }
+
+  .footer-link{
+    display: block;
+    margin: .25em 0;
+    padding-left: 2.5em;
+  }
+
+  .footer-spacer {
+    height: 8em;
   }
 
   .site-name{
