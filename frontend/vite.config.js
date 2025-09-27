38ccbcf7074a0369f76851e49f3a3bd3d8f24717
William Taylor <wcolet@gmail.com>
2025-09-27 15:00:04 -0500

    fixed hot reloading for development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+      interval: 1000,  // Poll every second
+      binaryInterval: 3000  // Important for WSL
+    },
+    hmr: {
+      clientPort: 5173  // Important when behind reverse proxy
+    }
+  }
 });
