166982abe987ee04feee644b4df2456c166a1bcc
William Taylor <wcolet@gmail.com>
2025-10-17 15:36:51 -0500

    removed unneeded api routes for template todo app

--- backend/server.js
+++ backend/server.js
@@ -28,62 +28,6 @@ app.get('/link/:shortCode', async (req, res) => {
 });
 
 
-// Get existing tasks from database
-app.get('/api/get_tasks', async (req, res) => {
-  try {
-    const { rows } = await query('SELECT * FROM todo_tasks');
-    res.json(rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
-
-// Update selected task status to complete
-app.put('/api/tasks/:id/complete', async (req, res) => {
-  const { id } = req.params;
-  const { is_completed } = req.body;
-
-  try {
-    const result = await query(
-      `UPDATE todo_tasks 
-       SET is_completed = $1, 
-           date_completed = CASE WHEN $1 THEN NOW() ELSE NULL END
-       WHERE id = $2
-       RETURNING *`,
-      [is_completed, id]
-    );
-
-    if (result.rows.length === 0) {
-      return res.status(404).json({ error: 'Task not found' });
-    }
-
-    res.json(result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: `Server error - ${id}` });
-  }
-});
-
-app.post('/api/submit_task', async (req,res) => {
-  const {newDesc} = req.body;
-
-  try {
-    const result = await query(
-      `INSERT INTO todo_tasks (description, date_added, date_completed, is_completed)
-      VALUES ($1, NOW(), NULL, FALSE)
-      RETURNING *`,
-      [newDesc]
-    );
-    res.json(result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: `Error submitting new task.` });
-
-  }
-});
-
-
 const PORT = process.env.BACKEND_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
