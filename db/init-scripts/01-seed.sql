7cce3a9111f43f1507ca0369e30cc32ac358f7b3
William Taylor <wcolet@gmail.com>
2025-11-11 22:14:42 -0600

    removed NASA site from monitored sites

--- db/init-scripts/01-seed.sql
+++ db/init-scripts/01-seed.sql
@@ -6,7 +6,6 @@ VALUES
 INSERT INTO monitored_services (name, url, description, base_url)
 VALUES
   ('weather.gov', 'https://api.weather.gov/alerts?limit=1', 'Site run by the National Weather Surface for reporting weather data.', 'https://weather.gov'),
-  ('NASA.gov', 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', 'Site for the US National Aeronautics and Space Administration.', 'https://nasa.gov'),
   ('Open Library', 'https://openlibrary.org/authors/OL448939A.json', 'Project under the Internet Archive to provide a universal book catalog.', 'https://openlibrary.org'),
   ('GitHub Pages', 'https://w-taylor.github.io/status.json', 'Custom endpoint for my GitHub Pages site (w-taylor.github.io/status.json)', 'https://w-taylor.github.io'),
   ('JSON Placeholder', 'https://jsonplaceholder.typicode.com/posts/1', 'Site for developers to get fake API data for testing and prototyping.', 'https://jsonplaceholder.typicode.com');
