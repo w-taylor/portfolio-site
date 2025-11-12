499397a9dca3d31214b8e86cf706090d933934df
William Taylor <wcolet@gmail.com>
2025-11-11 21:36:41 -0600

    tweaks to css and some text

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -17,7 +17,7 @@
     
     <div class="intro-box">
         <div class="intro-box-left text-blurb">
-            <p>Hello! I'm William, a full stack developer located in Austin, Texas. I love creating frontend user interfaces, developing backend scripts for business logic, and crafting efficient SQL queries. Take a look at my projects below if you want to see some of what I've been working on lately.</p>
+            <p>Hi, I'm William, a full-stack developer based in Austin.<br/><br/>I build dynamic user interfaces, robust backend systems, and efficient databases. I strive to create applications that are as intuitive for the user as they are technically sound under the hood. You can find a selection of some of my recent projects below.</p>
 
             <div style="display: flex;">Here's a link to my <a href="https://github.com/w-taylor" style="display: flex; align-items: center; justify-content: center;" target="_blank" rel="noopener noreferrer">&nbsp;&lt;<img src="/images/github-mark-white.png" style="height: 1em; width: 1em; vertical-align: middle;">&thinsp;Github&gt;</a></div>
             <br />
@@ -84,10 +84,12 @@
  ######## ###########     ###     ##########      ########### ###    #### ###        ########  
     </pre>
 
-    <p class="text-blurb">This site uses the Portable Site project as a template, with a Svelte frontend, Node.js api server, Postgres database, Nginx reverse-proxy, and deployed in a Docker container. Sveltekit was used for allowing multiple pages unlike the SPA setup in the Portable Template</p>
-    <p class="text-blurb">A lot of what was used here is probably overkill for a simple site like this, it's mostly an excuse to play with these different technologies.</p>
-    <p class="text-blurb">HTTPS is set up for this site using <a href="https://certbot.eff.org/" target="_blank" rel="noopener noreferrer">&lt;Certbot&gt;</a>, huge thanks to <a href="https://www.eff.org/" target="_blank" rel="noopener noreferrer">&lt;EFF&gt;</a> for maintaining the utility and <a href="https://letsencrypt.org/" target="_blank" rel="noopener noreferrer">&lt;Let's Encrypt&gt;</a> for providing the certificates.</p>
-    <p class="text-blurb">You can find the code for this site <a href="https://github.com/w-taylor/portfolio-site" target="_blank" rel="noopener noreferrer">&lt;here&gt;</a></p>
+    <p class="text-blurb">
+        This site uses the Portable Site project as a template, with a Svelte frontend, Node.js api server, Postgres database, Nginx reverse-proxy, and deployed in a Docker container. Sveltekit was used for allowing multiple pages unlike the SPA setup in the Portable Template <br /><br />
+        A lot of what was used here is probably overkill for a simple site like this, it's mostly an excuse to play with these different technologies.<br /><br />
+        HTTPS is set up for this site using <a href="https://certbot.eff.org/" target="_blank" rel="noopener noreferrer">&lt;Certbot&gt;</a>, huge thanks to <a href="https://www.eff.org/" target="_blank" rel="noopener noreferrer">&lt;EFF&gt;</a> for maintaining the utility and <a href="https://letsencrypt.org/" target="_blank" rel="noopener noreferrer">&lt;Let's Encrypt&gt;</a> for providing the certificates.<br /><br />
+        You can find the code for this site <a href="https://github.com/w-taylor/portfolio-site" target="_blank" rel="noopener noreferrer">&lt;here&gt;</a>
+    </p>
 
     <MainPageBreak />
 
@@ -101,7 +103,7 @@
  ########   ########  ###    ####     ### ###     ###  ########     ###     
     </pre>
 
-    <p class="text-blurb">If you'd like to get in touch or want to recieve a resume, you can email me at <a href="mailto:william@wtaylor.xyz">&lt;william@wtaylor.xyz&gt;</a></p>
+    <div style="display: flex; justify-content: center;"><p class="text-blurb">If you'd like to get in touch or want to recieve a resume, you can email me at&nbsp;<a href="mailto:william@wtaylor.xyz">&lt;william@wtaylor.xyz&gt;</a></p></div>
 
 </div>
 
