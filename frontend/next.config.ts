445d6a8a32192fd471c64efb5bac8ef4fe27e731
William Taylor <wcolet@gmail.com>
2026-03-10 14:51:31 -0500

    added initial changes for typescript adoption (typescript npm packages, swapped jsconfig.json for tsconfig.json, swapped next.config.js for next.config.ts)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   output: 'standalone',
   images: {
     formats: ['image/avif', 'image/webp'],
@@ -9,4 +10,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
